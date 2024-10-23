bbaa9e395e44cc9eb6110295ffec3ede306483d9
Aththariq Lisan Qur'an Daulah Sentono <130519479+aththariq@users.noreply.github.com>
2024-10-24 02:17:34 +0700

    fix bugs

--- js/main.js
+++ js/main.js
@@ -1,19 +1,46 @@
-window.addEventListener("scroll", function () {
-  var header = document.querySelector("header");
-  if (window.scrollY > 50) {
-    header.classList.add("scrolled");
-  } else {
-    header.classList.remove("scrolled");
-  }
-});
+function throttle(func, limit) {
+  let lastFunc;
+  let lastRan;
+  return function () {
+    const context = this;
+    const args = arguments;
+    if (!lastRan) {
+      func.apply(context, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(function () {
+        if (Date.now() - lastRan >= limit) {
+          func.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    }
+  };
+}
+
+window.addEventListener(
+  "scroll",
+  throttle(function () {
+    var header = document.querySelector("header");
+    if (window.scrollY > 50) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  }, 100)
+);
 
 const scrollingWrapper = document.querySelector(".scrolling-wrapper");
 const images = Array.from(scrollingWrapper.children);
 
 function duplicateImages() {
   const totalWidth = scrollingWrapper.scrollWidth;
+  const containerWidth = scrollingWrapper.clientWidth;
 
-  while (scrollingWrapper.scrollWidth < totalWidth * 2) {
+  const requiredDuplicates = Math.ceil(containerWidth / totalWidth);
+
+  for (let i = 0; i < requiredDuplicates; i++) {
     images.forEach((img) => {
       const clone = img.cloneNode(true);
       scrollingWrapper.appendChild(clone);
